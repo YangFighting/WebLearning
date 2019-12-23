7ee785696ed0d0fbd5f9feda38f0fad4590a70d3
YangFighting <yangzhangng_y@163.com>
2019-12-23 21:34:11 +0800

    [M] 使用带参数的修饰器实现路由

--- mini_web_dynamic/dynamic/mini_frame.py
+++ mini_web_dynamic/dynamic/mini_frame.py
@@ -1,23 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/20 20:56
+# @Author  : ZhangYang
+
+g_url_route = dict()
+
+
+# 带参数的修饰器
+def route(url):
+    def decorator(func):
+        g_url_route[url] = func
+
+        def warpper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        return warpper
+
+    return decorator
+
+
+@route("/index.py")
 def index():
     with open("./templates/index.html", encoding="utf-8") as f:
         content = f.read()
     return content
 
 
+@route("/center.py")
 def center():
     with open("./templates/center.html", encoding="utf-8") as f:
         return f.read()
-     
+
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
-    
+
     file_name = env['PATH_INFO']
     # file_name = "/index.py"
 
-    if file_name == "/index.py":
-        return index()
-    elif file_name == "/center.py":
-        return center()
-    else:
-        return 'Hello World! 我爱你中国....'
+    # if file_name == "/index.py":
+    #     return index()
+    # elif file_name == "/center.py":
+    #     return center()
+    # else:
+    #     return 'Hello World! 我爱你中国....'
+    try:
+        return g_url_route.get(file_name)()
+    except Exception as e:
+        return "route dict error: {}".format(e.__str__())
