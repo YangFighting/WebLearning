10b16bad781e0831d0f3b35177a6bf6be464b478
YangFighting <yangzhangng_y@163.com>
2020-03-23 22:49:50 +0800

    [M] sql 测试

--- SQL/sql_test.py
+++ SQL/sql_test.py
@@ -1,23 +1,30 @@
 from pymysql import *
+import psycopg2
 
 
-def main():
+def connet_mysql():
     # 创建Connection连接
-    conn = connect(host='47.97.124.83', port=3306, database='jingdong', user='root', password='123456', charset='utf8')
+    conn = connect( host='ip', port=3306, database='jingdong', user='root', password='password',
+                    charset='utf8' )
 
     # 获得Cursor对象
     cs1 = conn.cursor()
 
     # 执行select语句，并返回受影响的行数：查询一条数据
-    count = cs1.execute("""select id,name from goods where id>=4""")
+    count = cs1.execute( """select * from goods""" )
 
     # 打印受影响的行数
-    print("查询到%d条数据:" % count)
+    print( "查询到%d条数据:" % count )
 
     # 关闭Cursor对象
     cs1.close()
     # 关闭Connection对象
     conn.close()
+
+
+def main():
+    # 连接mysql
+    connet_mysql()
 
 
 if __name__ == "__main__":
